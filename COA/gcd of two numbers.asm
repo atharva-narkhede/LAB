78e7c9b8339f45c42cd5934058c1ecfce7b67a64
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2023-05-29 00:38:17 +0530

    Update gcd of two numbers.asm

--- COA/gcd of two numbers.asm
+++ COA/gcd of two numbers.asm
@@ -1,26 +1,27 @@
 org 100h
-.data
-a db 8
-b db 4
-hcf db ?
-.code
-mov ax,@data
-mov ds,ax
-mov al,a
-mov bl,b
-mov ah,0
-div bl
-cmp ah,0
-je exit
-jne l1
-l1:
-mov ah,0 
-mov al,a
-dec bl
-div bl
-cmp ah,0
-je exit
-jne l1
+
+mov ax, 8
+
+mov bx, 4
+
+gcd_loop:
+
+    cmp ax, bx
+
+    jz exit
+
+    jg greater
+
+    xchg ax, bx
+
+greater:
+
+    sub ax, bx
+
+    jmp gcd_loop
+
 exit:
-mov hcf,bl
-Hlt
+
+    mov cx, ax
+
+    Hlt
