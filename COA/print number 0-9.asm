4c2b466230bde26f89b3479ea7b5a7ce67eabec6
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2023-05-28 23:19:23 +0530

    Update print number 0-9.asm

--- COA/print number 0-9.asm
+++ COA/print number 0-9.asm
@@ -1,15 +1,34 @@
 org 100h
+
 .data
-a db 0,1,2,3,4,5,6,7,8,9
+
+    a db 0,1,2,3,4,5,6,7,8,9
+
 .code
-mov cx,10
-mov si,0 
-loop1:  
-mov al,a[si]
-mov dl,al
-add dl,48 
-mov ah,2h
-int 21h
-INC si
-loop loop1
-Hlt
+
+    mov cx, 10
+
+    mov si, 0
+
+loop1:
+
+    mov dl, a[si]
+
+    add dl, '0'
+
+    mov ah, 2h
+
+    int 21h
+
+    inc si
+
+    loop loop1
+
+    mov ah, 4Ch
+
+    int 21h
+
+
+
+
+
