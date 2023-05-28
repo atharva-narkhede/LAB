41257aa5d5fe47235eac504b6ee1d46aca8530a6
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2023-05-28 23:47:23 +0530

    Update add two 16 bit numbers.asm

--- COA/add two 16 bit numbers.asm
+++ COA/add two 16 bit numbers.asm
@@ -1,16 +1,21 @@
 org 100h
+.data
+num1 dw 1234h
+num2 dw 5678h
+sum  dw 0
+.code
+mov ax, @data
+mov ds, ax
+mov ax, num1
+add ax, num2
+mov sum, ax
+hlt
 
-section .data
-    num1 dw 1234h   ; First number (in hexadecimal)
-    num2 dw 5678h   ; Second number (in hexadecimal)
-    result dw ?     ; Variable to store the result
 
-section .text
-    mov ax, [num1]    ; Move the value of num1 into AX register
-    add ax, [num2]   ; Add the value of num2 to AX register
-    mov [result], ax  ; Store the result in the result variable
+----------------------------
 
-    mov ah, 4Ch       ; Function to terminate the program
-    int 21h           ; Terminate the program
-
-end
+org 100h
+.code
+mov ax, 1234h
+add ax, 5678h
+hlt
